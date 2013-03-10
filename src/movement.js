706b77609422a48d55cd003b8dd7664ee6c6e493
Minko Gechev <mgechev@gmail.com>
2013-03-10 14:11:49 +0200

    Few minor changes

--- src/movement.js
+++ src/movement.js
@@ -135,7 +135,7 @@
         lastMovement = undefined,
         framesWithoutMotion = 0,
         getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia,
-        URL = URL || webkitURL || mozURL;
+        URL = window.URL || window.webkitURL || window.mozURL;
 
     Movement.init = function (options) {
         var self = this;
@@ -163,7 +163,8 @@
     Movement._initCanvases = function () {
         can = document.createElement('canvas');
         document.body.appendChild(can);
-        can.className = 'movementjs-main';
+        can.id = 'movementjs-main-canvas';
+        can.style.position = 'absolute';
         can.style.visibility = 'visible';
 
         background = document.createElement('canvas');
